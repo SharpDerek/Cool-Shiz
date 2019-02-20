de5afca7fc10b799326ff4eb65775f812b7f2f43
SharpDerek <derek.sharp@madwiremedia.com>
2019-02-20 11:15:42 -0700

    Create resizer.js

---
+++ JS/Grid Item Resizer/resizer.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+  $('simple-example').uniformSize();
+  $('breakpoint-example').uniformSize({
+    'breakpoint':768
+  });
+  $('breakpoint-and-max-height-example').uniformSize({
+    'breakpoint':768,
+    'max_height': '300px'
+  });
+});
