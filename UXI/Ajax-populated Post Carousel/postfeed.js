4f50c08af67f7a92208f7315453573de97077b44
SharpDerek <derek.sharp@madwiremedia.com>
2019-05-10 08:55:39 -0600

    Update postfeed.js
    Feed now grabs <title> tag of posts, and will automatically remove broken ones so that it's not just a perpetually spinning icon. Broken posts are logged as errors in the console so they can be checked.

--- UXI/Ajax-populated Post Carousel/postfeed.js
+++ UXI/Ajax-populated Post Carousel/postfeed.js
@@ -1,22 +1,29 @@
 var ajaxEntries = [];
 jQuery('head').append('<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>');
 jQuery(document).ready(function($) {
+    var feedIndex = 0;
    $('.ds-post-feed').each(function() {
      var columns = parseInt($(this).attr('columns'));
      var slugs = $(this).attr('slugs').split(",");
      var titles = $(this).attr('titles');
      var category = $(this).attr('category');
      var thisFeed = $(this);
+     var itemIndex = 0;
+     thisFeed.attr("feed-index",feedIndex);
      slugs.forEach(function(slug,index) {
          var feedItem = $('<div class="ds-feed-item ' + category + ' ' + slug.replace(/\//g,"-") + '"></div>');
          thisFeed.append(feedItem);
          var newAjaxEntry = {
              url: window.location.protocol + "//" + window.location.hostname + (category ? '/' + category + '/' : '/') + slug,
              location: feedItem,
-             title: titles
+             title: titles,
+             feedindex: feedIndex,
+             itemindex: itemIndex
          }
          ajaxEntries.push(newAjaxEntry);
+         itemIndex++;
      });
+     feedIndex++;
      thisFeed.slick({
         slidesToShow: columns,
         slidesToScroll: columns,
@@ -51,12 +58,18 @@ function loadAjaxEntry(index) {
       success: function(data) {
           var atag = $('<a class="post-feed-link" href="' + entry.url + '"></a>');
           atag.css('background-image','url(' + $(data).find('.post-image').find('img').attr('src') + ')');
-          var title = $(data).find('.page-header-title').html();
-          if (entry.title === 'partial') {
-              title = title.split(":")[0];
+          var title = $(data).filter('title').html();
+          if (title !== undefined && entry.title === 'partial') {
+              title = title.split(":")[0].split("|")[0].trim();
           }
-          atag.append('<h2>' + title + '</h2>');
+          if (title !== undefined) {
+              atag.append('<h2>' + title + '</h2>');
+          }
           entry.location.append(atag);
+      },
+      error: function() {
+          console.error("Could not load feed item at " + ajaxEntries[index].url + " (item " + ajaxEntries[index].itemindex + ") in slider " + ajaxEntries[index].feedindex + ". This entry will be removed from the feed.");
+          $('.ds-post-feed[feed-index="' + ajaxEntries[index].feedindex + '"]').slick('slickRemove',ajaxEntries[index].itemindex);
       },
       complete: function() {
           index++;
