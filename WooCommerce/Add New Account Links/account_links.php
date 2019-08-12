edf22a5f61df61133643e6b9e160ab0c63c713e6
SharpDerek <derek.sharp@madwiremedia.com>
2019-08-12 11:29:48 -0600

    Create account_links.php

---
+++ WooCommerce/Add New Account Links/account_links.php
@@ -0,0 +1,26 @@
+	function woo_new_account_links( $menu_links ){
+
+		$new = array( 'link-slug' => 'Link Name' );
+
+		$menu_links = array_slice( $menu_links, 0, 2, true ) 
+		+ $new 
+		+ array_slice( $menu_links, 1, NULL, true );
+	 
+		return $menu_links;
+	 
+	}
+	add_filter ( 'woocommerce_account_menu_items', 'woo_new_account_links' );
+
+
+	function woo_new_account_link_endpoints( $url, $endpoint, $value, $permalink ){
+	 
+		if( $endpoint === 'link-slug' ) {
+	 
+			$url = "----link-url-----";
+	 
+		}
+		return $url;
+	 
+	}
+	 
+	add_filter( 'woocommerce_get_endpoint_url', 'woo_new_account_link_endpoints', 10, 4 );
